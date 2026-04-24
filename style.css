9b1928aea83598cd4857f8476d18788248259dea
nitya-anand <nityaanand209@gmail.com>
2026-04-24 13:09:15 -0400

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,32 @@
+body {
+  background-color: black;
+  color: white;
+  font-family: Arial, sans-serif;
+  margin: 0;
+}
 
+.container {
+  padding: 80px;
+}
+
+.name {
+  font-size: 80px;
+  font-weight: bold;
+}
+
+.menu {
+  margin-top: 40px;
+}
+
+.menu a {
+  display: block;
+  font-size: 28px;
+  color: white;
+  text-decoration: none;
+  margin: 10px 0;
+  transition: 0.3s;
+}
+
+.menu a:hover {
+  opacity: 0.5;
+}
